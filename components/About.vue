f85ebfcacfb99f2c36b09151f71750feab78de76
laura <tundelaura93@gmail.com>
2020-06-13 19:59:16 +0100

    wip

--- components/About.vue
+++ components/About.vue
@@ -8,7 +8,28 @@
         <Nav color="#e0ebe8"></Nav>
       </div>
     </div>
-    <div class="half-container"></div>
+    <div class="half-container description-container">
+      <img src="../assets/laura_ca2.png" class="rounded-img" />
+      <p class="about-description">
+        I’m a software engineer, currently based in London, looking to pursue a
+        career in full-stack development, to further develop my capabilities in
+        the latest web and mobile technologies.
+        <br /><br />
+        After graduating from the University of Birmingham, I worked in finance
+        and consulting in international businesses based out of New York. Having
+        progressed quickly, I made the decision to leave the world of finance
+        and make a career change into the multifaceted environment of
+        technology.
+        <br /><br />
+        I recently completed an advanced coding bootcamp (Progmasters), where I
+        thrived under the pressure of constantly learning new technologies and
+        solving complex problems.
+        <br /><br />
+        My main technologies are
+        <strong>Python, Java Spring, VueJS </strong> and
+        <strong>Angular</strong>.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -21,6 +42,14 @@ export default {
 }
 </script>
 <style scoped>
+.rounded-img {
+  height: 250px;
+  width: 250px;
+  border-radius: 12%;
+  display: inline-block;
+  margin-bottom: 40px;
+}
+
 .header {
   display: flex;
   flex-direction: column;
@@ -45,11 +74,20 @@ h1 {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
 }
 .left-container {
   justify-content: space-between;
 }
 .green {
   background-color: #45b29a;
+}
+.description-container {
+  padding: 0 200px;
+}
+.about-description {
+  font-size: 1.2em;
+  text-align: left;
+  display: inline;
 }
 </style>
